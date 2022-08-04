86659a84cb0c1589fc2fc548632bfe95ab15fb8c
bl00p1ng <blooping@protonmail.com>
2022-08-04 16:25:07 -0500

    Formulario de registro pacientes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,102 @@
 const Form = () => {
   return (
-    <>
-        <h2>Formulario</h2>
-    </>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
+
+      <p className="text-lg mt-5 mb-10 text-center">
+        Añade Pacientes y {''}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+
+      {/* FORMULARIO REGISTRO PACIENTES */}
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        {/* NOMBRE MASCOTA */}
+        <div className="mb-5">
+          <label 
+            className="block text-gray-700 uppercase font-bold" 
+            htmlFor="petName"
+          >
+            Nombre Mascota
+          </label>
+          <input 
+            id="petName"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="text"
+            placeholder="Nombre de la Mascota"
+          />
+        </div>
+
+        {/* NOMBRE PROPIETARIO */}
+        <div className="mb-5">
+          <label 
+            className="block text-gray-700 uppercase font-bold" 
+            htmlFor="ownerName"
+          >
+            Nombre Propietario
+          </label>
+          <input 
+            id="ownerName"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="text"
+            placeholder="Nombre del Propietario"
+          />
+        </div>
+
+        {/* EMAIL PROPIETARIO */}
+        <div className="mb-5">
+          <label 
+            className="block text-gray-700 uppercase font-bold" 
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input 
+            id="email"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="email"
+            placeholder="Email de Contacto"
+          />
+        </div>
+
+        {/* FECHA DE ALTA */}
+        <div className="mb-5">
+          <label 
+            className="block text-gray-700 uppercase font-bold" 
+            htmlFor="date"
+          >
+            Fecha de Alta
+          </label>
+          <input 
+            id="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="date"
+          />
+        </div>
+
+        {/* SINTOMAS */}
+        <div className="mb-5">
+          <label 
+            className="block text-gray-700 uppercase font-bold" 
+            htmlFor="symptoms"
+          >
+            Síntomas
+          </label>
+          <textarea 
+            name="symptoms" 
+            id="symptoms"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Describe los síntomas"
+          />
+        </div>
+
+        {/* ENVIAR FORM */}
+        <input 
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white rounded-md uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+          value="Agregar paciente"
+        />
+      </form>
+    </div>
   )
 }
 
--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,8 +1,8 @@
 const PatientsList = () => {
   return (
-    <>
+    <div className="w-1/2 lg:3/5">
       <h2>Listado Pacientes</h2>
-    </>
+    </div>
   )
 }
 
